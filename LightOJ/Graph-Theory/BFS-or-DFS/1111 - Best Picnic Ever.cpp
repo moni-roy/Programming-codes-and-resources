a9a3aa1016d7d637cbdfea935d66aa480b25fea4
Monikrishna Roy <roy.moni.ju.cse@gmail.com>
2015-03-30 02:14:09 +0600

    Update source code

--- LightOJ/Graph-Theory/BFS-or-DFS/1111 - Best Picnic Ever.cpp
+++ LightOJ/Graph-Theory/BFS-or-DFS/1111 - Best Picnic Ever.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long 			ll;
+typedef long long 		ll;
 typedef long long unsigned 	llu;
-typedef double 				dl;
+typedef double 			dl;
 typedef pair<int,int> 		pii;
 typedef pair<ll,ll> 		pll;
 typedef vector<int> 		vi;
 typedef map<int,int> 		mii;
-typedef map<ll,ll> 			mll;
+typedef map<ll,ll>		mll;
 typedef map<string,int> 	msi;
 typedef map<char,int> 		mci;
 
-#define PI			acos(-1.0)
-#define sn	 		second
-#define fs 			first
-#define pb			push_back
-#define mp			make_pair
+#define PI		acos(-1.0)
+#define sn 		second
+#define fs		first
+#define pb		push_back
+#define mp		make_pair
 #define zero(a) 	memset(a,0,sizeof a)
 #define minus(a) 	memset(a,-1,sizeof a)
 #define setinf(a) 	memset(a,126,sizeof a)
@@ -24,9 +24,9 @@ typedef map<char,int> 		mci;
 #define FRV(i,x,y) 	for(int i=x;i>=y;i--)
 #define all(v)		(v.begin(),v.end())
 #define vsort(v)	sort(all(v))
-#define nl 			puts("")
+#define nl 		puts("")
 #define endl		'\n'
-#define I_O			ios_base::sync_with_stdio(0); cin.tie(0);
+#define I_O		ios_base::sync_with_stdio(0); cin.tie(0);
 
 template <class T> inline T BMOD(T p,T e,T m){T ret=1;while(e){if(e&1) ret=(ret*p)%m;p=(p*p)%m;e>>=1;}return (T)ret;}
 template <class T> inline T MODINV(T a,T m){return BMOD(a,m-2,m);}
