60c258e55844884cf023297c60e1e926f3c50b79
moni-roy <roy.moni.ju.cse@gmail.com>
2015-05-31 01:12:31 +0600

    Just implementation

---
+++ LightOJ/Greedy/1389 - Scarecrow.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long          	ll;
+typedef long long unsigned 	llu;
+typedef double             	dl;
+typedef pair<int,int>      	pii;
+typedef pair<ll,ll>     	pll;
+typedef vector<int>        	vi;
+typedef vector<ll>        	vl;
+typedef map<int,int> 		mii;
+typedef map<ll,ll>         	mll;
+typedef map<string,int>    	msi;
+typedef map<char,int>      	mci;
+
+#define PI          acos(-1.0)
+#define S           second
+#define F           first
+#define PB          push_back
+#define MP          make_pair
+#define P           push
+#define zero(a)     memset(a,0,sizeof a)
+#define minus(a)    memset(a,-1,sizeof a)
+#define setinf(a)   memset(a,126,sizeof a)
+#define all(v)      (v.begin(),v.end())
+#define vsort(v)    sort(v.begin(),v.end())
+#define nl          puts("")
+#define tcase(cs)   printf("Case %d:",++cs)
+#define endl        '\n'
+#define READ(in)    freopen("in.in","r",stdin)
+#define WRITE(out)  freopen("out.out","w",stdout)
+#define I_O         ios_base::sync_with_stdio(0); cin.tie(0);
+#define gcd(a,b)    __gcd(a,b)
+
+//cout << fixed << setprecision(20) << p << endl;
+
+template <class T> inline T BMOD(T p,T e,T m){T ret=1;while(e){if(e&1) ret=(ret*p)%m;p=(p*p)%m;e>>=1;}return (T)ret;}
+template <class T> inline T MODINV(T a,T m){return BMOD(a,m-2,m);}
+
+int ts,cs=0,n,sv[1001],cnt;
+string s;
+int main()
+{
+	cin>>ts;
+	while(ts--){
+		cin>>n;
+		cin>>s;
+		s+='#';
+		cnt=0;
+		zero(sv);
+		//sv[0]=1;
+		for(int i=1;i<=n;i++){
+			if(s[i-1]=='.'&&sv[i-1]==0){
+				cnt++;
+				sv[i-1]=sv[i]=sv[i+1]=1;
+			}
+			else{
+				sv[i-1]=1;
+			}
+		}
+		cout<<"Case "<<++cs<<": "<<cnt<<endl;
+	}
+	return 0;
+}
