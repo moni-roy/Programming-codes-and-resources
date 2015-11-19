09ba95714013e5f0a37557d2b7db98d6902d91ac
moni-roy <roy.moni.ju.cse@gmail.com>
2015-11-20 03:24:49 +0600

    Discrete Logarithm/Shank`s Baby-step Giant-step Algorithm

---
+++ Discrete Logarithm_Shank`s Baby-step Giant-step Algorithm/1325 - Distributing Chocolates.cpp
@@ -0,0 +1,142 @@
+/* * * * * * * * * * * * * * *
+ * :Krishna (MRoy)
+ * :JU
+ * :mkroy.cs@gmail.com
+ * * * * * * * * * * * * * * */
+#include <bits/stdc++.h>
+//~ c
+#include <cctype>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cassert>
+#include <climits>
+//~ c++
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <iomanip>
+#include <numeric>
+#include <iterator>
+#include <typeinfo>
+#include <valarray>
+#include <functional>
+
+using namespace std;
+
+typedef long long          	ll;
+typedef long long unsigned 	llu;
+typedef double             	dl;
+typedef pair<int,int>      	pii;
+typedef pair<ll,ll>     	pll;
+typedef vector<int>        	vi;
+typedef vector<ll>        	vl;
+typedef map<int,int> 		mii;
+typedef map<ll,ll>         	mll;
+typedef map<string,int>    	msi;
+typedef map<char,int>      	mci;
+typedef pair<int ,pii>      piii;
+typedef vector<pii>         vpii;
+typedef vector<piii>        vpiii;
+//~ Define
+#define pi                  acos(-1.0)
+#define S                   second
+#define F                   first
+#define pb                  push_back
+#define MP                  make_pair
+#define P                   push
+#define zero(a)             memset(a,0,sizeof a)
+#define minus(a)            memset(a,-1,sizeof a)
+#define setinf(a)           memset(a,126,sizeof a)
+#define all(v)              v.begin(),v.end()
+#define vsort(v)            sort(v.begin(),v.end())
+#define I_O                 ios_base::sync_with_stdio(0); cin.tie(0)
+#define IO                  ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define gcd(a,b)            __gcd(a,b)
+//~ Input                   
+#define I(a)                scanf("%d",&a)
+#define I2(a,b)             scanf("%d%d",&a,&b)
+#define I3(a,b,c)           scanf("%d%d%d",&a,&b,&c)
+#define L(a)                scanf("%lld",&a)
+#define L2(a,b)             scanf("%lld%lld",&a,&b)
+#define L3(a,b,c)           scanf("%lld%lld%lld",&a,&b,&c)
+#define D(a)                scanf("%lf",&a)
+#define sc                  scanf
+//~ Output                  
+#define nl                  puts("")
+#define sp                  printf(" ");
+#define SP                  cout<<" ";
+#define tcase(cs)           printf("Case %d:",++cs)
+#define TC(cs)              cout<<"Case "<<++cs<<":"
+#define PI(a)               printf("%d",a)
+#define PL(a)               printf("%lld",a)
+#define PD(a)               printf("%lf",a)
+#define pr                  printf
+
+template <class T> inline T BMOD(T p,T e,T m){T ret=1;while(e){if(e&1) ret=(ret*p)%m;p=(p*p)%m;e>>=1;}return (T)ret;}
+template <class T> inline T MODINV(T a,T m){return BMOD(a,m-2,m);}
+template <class T> inline T isPrime(T a){for(T i=2;i<=sqrt(double(a));i++){if(a%i==0){return 0;}}return 1;}
+template <class T> inline T lcm(T a, T b){return (a/gcd(a,b))*b;}
+template <class T> inline T power(T a, T b){return (b==0)?1:a*power(a,b-1);}
+
+#define READ(in)    freopen("in.in","r",stdin)
+#define WRITE(out)  freopen("out.out","w",stdout)
+
+//~ cout << fixed << setprecision(20) << Ans << endl;
+//~ priority_queue<piii,vpiii, greater<piii> >pq; //for dijkstra
+
+/******************************Let's GO*********************************/
+#define md 100000007ll
+ll ts,cs=0,a,b,Ans,an,n;
+mll val;
+
+int main()
+{
+	IO
+	cin>>ts;
+	while(ts--){
+		cin>>a>>b;
+		n=(ll)sqrt(md+.0);
+		an=1;
+		for(int i=0;i<n;++i){
+			an=(an*a)%md;
+		}
+		val.clear();
+		for(int i=1,cr=an;i<=n;++i){
+			if(!val.count(cr)){
+				val[cr]=i;
+			}
+			cr=(cr*an)%md;
+		}
+		ll ans=md;
+		for(int i=0,cr=b;i<=n;++i){
+			if(val.count(cr)){
+				Ans=val[cr]*n-i;
+				if(Ans<md){
+					ans=min(ans,Ans);
+				}
+			}
+			cr=(cr*a)%md;
+		}
+		TC(cs),SP;
+		cout<<ans<<endl;
+	}
+	return 0;
+}
+
+//http://e-maxx-eng.github.io/algebra/discrete-log.html
