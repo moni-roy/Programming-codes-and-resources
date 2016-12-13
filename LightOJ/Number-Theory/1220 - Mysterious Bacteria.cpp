b782b780836c1ba777df238064972d820483f6af
moni-roy <roy.moni.ju.cse@gmail.com>
2016-12-13 23:14:28 +0600

    LightOJ 1220 - Mysterious Bacteria

---
+++ LightOJ/Number-Theory/1220 - Mysterious Bacteria.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long          	ll;
+typedef long long unsigned  llu;
+typedef double             	dl;
+typedef pair<int,int>      	pii;
+typedef pair<ll,ll>     	pll;
+typedef vector<int>        	vi;
+typedef vector<ll>        	vl;
+typedef map<int,int> 		mii;
+typedef map<ll,ll>         	mll;
+typedef map<string,int>    	msi;
+typedef map<char,int>      	mci;
+typedef pair<int ,pii>      piii;
+typedef vector<pii>         vpii;
+typedef vector<piii>        vpiii;
+
+#define pi                  acos(-1.0)
+#define S                   second
+#define F                   first
+#define pb                  push_back
+#define MP                  make_pair
+#define mem(a,b)            memset(a,b,sizeof a)
+#define all(v)              v.begin(),v.end()
+#define vsort(v)            sort(v.begin(),v.end())
+#define IO                  ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define gcd(a,b)            __gcd(a,b)
+
+#define I(a)                scanf("%d",&a)
+#define I2(a,b)             scanf("%d%d",&a,&b)
+#define L(a)                scanf("%lld",&a)
+#define L2(a,b)             scanf("%lld%lld",&a,&b)
+#define D(a)                scanf("%lf",&a)
+
+#define SP                  cout<<" ";
+#define PI(a)               printf("%d",a)
+#define PL(a)               printf("%lld",a)
+#define PD(a)               printf("%lf",a)
+#define CHK                 cout<<"MK"<<endl
+
+template <class T> inline T BMOD(T p,T e,T m){T ret=1;while(e){if(e&1) ret=(ret*p)%m;p=(p*p)%m;e>>=1;}return (T)ret;}
+template <class T> inline T MODINV(T a,T m){return BMOD(a,m-2,m);}
+template <class T> inline T isPrime(T a){for(T i=2;i<=sqrt(double(a));i++){if(a%i==0){return 0;}}return 1;}
+template <class T> inline T lcm(T a, T b){return (a/gcd(a,b))*b;}
+template <class T> inline T power(T a, T b){return (b==0)?1:a*power(a,b-1);}
+template <class T> inline string toStr(T t) { stringstream ss; ss<<t; return ss.str();}
+template <class T> inline long long toLong(T t) {stringstream ss;ss<<t;long long ret;ss>>ret;return ret;}
+template <class T> inline int toInt(T t) {stringstream ss;ss<<t;int ret;ss>>ret;return ret;}
+
+//~ cout << fixed << setprecision(20) << Ans << endl;
+//~ priority_queue<piii,vpiii, greater<piii> >pq; //for dijkstra
+/*                 _                      */
+/*____________|\/||_||\||_________________*/
+
+string buffer;
+int INT(){
+	getline(cin,buffer);
+	return toInt(buffer);
+}
+int LONG(){
+	getline(cin,buffer);
+	return toLong(buffer);
+}
+
+#define MX 1000010
+#define MD 1e9+7
+
+const int N = 1<<16;
+int lp[N+1];
+vector<int> pr;
+
+void sieve(){
+	for (int i=2; i<=N; ++i) {
+		if (lp[i] == 0) {
+			lp[i] = i;
+			pr.push_back (i);
+		}
+		for (int j=0; j<(int)pr.size() && pr[j]<=lp[i] && i*pr[j]<=N; ++j)
+			lp[i * pr[j]] = pr[j];
+	}
+}
+
+int main()
+{
+	IO;
+	freopen("/home/krishna/PRG/practice/input.in","r",stdin);
+	sieve();
+	int ts,cs=0;
+	ll n;
+	cin>>ts;
+	while(ts--){
+		cin>>n;
+		int sign = 0;
+		if(n<0){
+			n=-n;
+			sign = 1;
+		}
+		int gd =0,Ans =0;
+		for(int i=0;i<(int)pr.size() && pr[i]*pr[i]<=n;i++){
+			if(n%pr[i]==0){
+				int cnt = 0;
+				while(n%pr[i]==0){
+					n/=pr[i];
+					cnt++;
+				}
+				gd = gcd(cnt,gd);
+			}
+		}
+		if(n>1){
+			Ans = 1;
+		}
+		else{
+			Ans = gd;
+			if(sign){
+				while((Ans&1)^1){
+					Ans/=2;
+				}
+			}
+		}
+		cout<<"Case "<<++cs<<": "<<Ans<<endl;
+	}
+	return 0;
+}
+
+	
+
