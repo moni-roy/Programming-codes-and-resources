e52599aaf0432922188da4acb5cc7efcfcd8a3b8
moni-roy <roy.moni.ju.cse@gmail.com>
2020-05-01 14:18:59 +0600

    [Week-1 Day-1] May LeetCoding Challenge
    - Problem: 278. First Bad Version
    - Date: May 1, 2020

---
+++ Online Judges/LeetCode/May LeetCoding Challenge/Week-1/First Bad Version.py
@@ -0,0 +1,25 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l = 1
+        r = n
+        Ans = 0
+        while l<=r:
+            m = (l+r)/2
+            m = int(m)
+            c = isBadVersion(m)
+            if(c) :
+                Ans = m
+                r = m-1
+            else:
+                l = m+1
+        return Ans
+        
