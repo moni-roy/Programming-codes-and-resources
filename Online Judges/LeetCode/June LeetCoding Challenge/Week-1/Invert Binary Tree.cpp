bd7163eba8803bacf4e906b8ac5b51c8be26c4e4
moni-roy <roy.moni.ju.cse@gmail.com>
2020-06-01 21:43:24 +0600

    [Week-1 Day-1] June LeetCoding Challenge
    - Problem: 226. Invert Binary Tree
    - Date: June 1, 2020

---
+++ Online Judges/LeetCode/June LeetCoding Challenge/Week-1/Invert Binary Tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == NULL)
+            return root;
+        if (root->left != NULL)
+            invertTree(root->left);
+        if (root->right != NULL)
+            invertTree(root->right);
+        swap(root->left, root->right);
+        return root;
+    }
+};
